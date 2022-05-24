93b628f50d132b87fe0c7a672c301c0274cb76a5
Cao Minh Tri <caominhtri112201@gmail.com>
2022-05-24 20:32:40 +0700

    Updates

---
+++ precache-manifest.278bcfc57dd182ad79b0f86a5c617284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a606710c094008ef736e788ce85f5b1",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "cab92e8d588e37c4bae5",
+    "url": "/grocery-bud/static/css/main.99e9fbee.chunk.css"
+  },
+  {
+    "revision": "a5640d4bcbc7f4184abc",
+    "url": "/grocery-bud/static/js/2.79ac4408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.79ac4408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab92e8d588e37c4bae5",
+    "url": "/grocery-bud/static/js/main.b1e5e5f3.chunk.js"
+  },
+  {
+    "revision": "f19ce6e521be50b493db",
+    "url": "/grocery-bud/static/js/runtime-main.8b040e9c.js"
+  }
+]);
